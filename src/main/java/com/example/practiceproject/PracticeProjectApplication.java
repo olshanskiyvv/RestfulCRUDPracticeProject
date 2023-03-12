605766f73b8df33c2e4a475eac553865f5fafa67
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2023-03-12 15:55:23 +0300

    Fix CORS problem

--- src/main/java/com/example/practiceproject/PracticeProjectApplication.java
+++ src/main/java/com/example/practiceproject/PracticeProjectApplication.java
@@ -2,7 +2,8 @@ package com.example.practiceproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PracticeProjectApplication {
