5fdbc53f71bc2da0861c67669d364aab4d183034
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2023-03-22 19:33:47 +0300

    Add async and add Dockerfile for backend

--- backend_server/src/main/java/com/example/practiceproject/service/Service.java
+++ backend_server/src/main/java/com/example/practiceproject/service/Service.java
@@ -3,7 +3,10 @@ package com.example.practiceproject.service;
 import com.example.practiceproject.model.Weapon;
 import com.example.practiceproject.repository.IRepository;
 import com.example.practiceproject.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import java.io.File;
 import java.util.List;
@@ -11,10 +14,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @org.springframework.stereotype.Service
 public class Service implements IService {
-    private static final IRepository WEAPON_REPOSITORY = new Repository();
-    private static AtomicInteger WEAPON_ID_HOLDER = new AtomicInteger();
+    private static IRepository WEAPON_REPOSITORY;
+    private static AtomicInteger WEAPON_ID_HOLDER;
 
-    public Service() {
+    @Autowired
+    public Service(Repository iRepository) {
+        WEAPON_REPOSITORY = iRepository;
         List<Weapon> weaponList = WEAPON_REPOSITORY.readAll();
         Integer maxId = 0;
         for (Weapon weapon : weaponList) {
@@ -26,6 +31,7 @@ public class Service implements IService {
     }
 
     @Override
+    @Async
     public void create(Weapon weapon) {
         final int weaponSerialNumber = WEAPON_ID_HOLDER.incrementAndGet();
         weapon.setSerialNumber(weaponSerialNumber);
@@ -33,31 +39,37 @@ public class Service implements IService {
     }
 
     @Override
+    @Async
     public Weapon read(Integer serial_number) {
         return WEAPON_REPOSITORY.read(serial_number);
     }
 
     @Override
+    @Async
     public List<Weapon> readAll() {
         return WEAPON_REPOSITORY.readAll();
     }
 
     @Override
+    @Async
     public boolean update(Weapon weapon, Integer serial_number) {
         return WEAPON_REPOSITORY.update(weapon, serial_number);
     }
 
     @Override
+    @Async
     public boolean delete(Integer serial_number) {
         return WEAPON_REPOSITORY.delete(serial_number);
     }
 
     @Override
+    @Async
     public File getData() {
         return WEAPON_REPOSITORY.getData();
     }
 
     @Override
+    @Async
     public boolean setData(MultipartFile file) {
         return WEAPON_REPOSITORY.setData(file);
     }
