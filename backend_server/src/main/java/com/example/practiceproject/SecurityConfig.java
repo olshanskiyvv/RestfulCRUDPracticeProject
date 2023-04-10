2511b180443a33fc5c3707ed9f4da47858cd0df8
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2023-04-10 15:12:16 +0300

    Update SecurityConfig
    Delete CORS disabling and all user authorization

--- backend_server/src/main/java/com/example/practiceproject/SecurityConfig.java
+++ backend_server/src/main/java/com/example/practiceproject/SecurityConfig.java
@@ -2,22 +2,10 @@ package com.example.practiceproject;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .authorizeHttpRequests().requestMatchers("/**").permitAll()
-                .requestMatchers("/**").permitAll()
-                .anyRequest().authenticated();
-
-        return http.build();
-    }
-
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/**");
